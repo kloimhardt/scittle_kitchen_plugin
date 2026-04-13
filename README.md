72228270dbf4a93df21b76305365bafc01792ab0
kloimhardt <markus.agwin@gmx.net>
2026-04-13 13:41:12 +0200

    custom function in spreadsheet

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # scittle_kitchen_plugin
+Tutorial
+
+https://api.onlyoffice.com/docs/plugin-and-macros/samples/plugins/hello-world/
+https://api.onlyoffice.com/docs/office-api/get-started/overview/
+
+```
+python cors_server.py
+```
+
+In browser developer console, on the right bottom switch from `top` to  `new.docx` (otherwise below you get `ReferenceError: Asc is not defined`)
+
+```
+fetch("http://localhost:8003/config.json")
+.then(function(response) {
+  return response.json();
+})
+.then(function(data) {
+  console.log(data);
+});
+
+Asc.editor.installDeveloperPlugin("http://localhost:8003/config.json")
+```
+generate uuid: `$ uuidgen`
+
+`Plugins` appears next to `View`
--- scripts/helloworld.js
+++ scripts/helloworld.js
@@ -1,22 +1,3 @@
-/**
- *
- * (c) Copyright Ascensio System SIA 2020
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-// Example insert text into editors (different implementations)
 (function(window, undefined){
 
     scittle.core.eval_string("(.log js/console 'scittle1')");
@@ -25,17 +6,35 @@
 
     scittle.core.eval_string("(.log js/console (->infix ((D sin) 'x)))");
 
-    var text = "Hello world4";
+    var text = "Hello world6";
 
     window.Asc.plugin.init = function()
     {
         // call command with external variables
         Asc.scope.text = text; // export variable to plugin scope
+        Asc.scope.emmy_eval = (s) => scittle.core.eval_string(s);
+        console.log(77);
+        console.log(Asc.scope.emmy_eval("(->infix ((D cos) 'x))"));
         this.callCommand(function() {
-            var oDocument = Api.GetDocument();
-            var oParagraph = Api.CreateParagraph();
-            oParagraph.AddText(Asc.scope.text); // or oParagraph.AddText(scope.text);
-            oDocument.InsertContent([oParagraph]);
+	          /**
+	           * Function that returns the argument
+	           * @customfunction
+	           * @param {any} arg Any data.
+             * @returns {any} The argumet of the function.
+	           */
+            function myf1(arg) {
+	              return 4 * arg;
+	          }
+
+            var activeSheet = Api.GetActiveSheet();
+            activeSheet.GetRange("A1").Select();
+            activeSheet.GetRange("A1").SetValue(myf1(2));
+            activeSheet.GetRange("B1").SetValue(Asc.scope.text);
+            activeSheet.GetRange("C1").SetValue(Asc.scope.emmy_eval);
+            console.log(Asc.scope.emmy_eval);
+
+	          Api.AddCustomFunction(myf1);
+
         }, true);
     };
 
